320b73fc6e6e609b459140fc0a8526a78f7daed5
Andrea Fernandez Conde <andrea.fernandez.conde@gmail.com>
2013-07-29 09:53:43 -0500

    Added measure difference from periodic means

--- Code/get_stats.py
+++ Code/get_stats.py
@@ -1,6 +1,7 @@
 import sys
 from utils import data_loc, fig_loc, qload, qdump, interp, clean
 import numpy as np
+from plotter_new import *
 from datetime import datetime
 import time as itime
 import matplotlib.pyplot as plt
@@ -125,8 +126,28 @@ def get_stats(d):
           report += "\t" + highest_date.strftime("%m/%d/%Y %H:%M:%S") + " -> " + str(highest_val) + "kwh\n"
      return report
 
+def diff_mean(b,p):
+    """Calcualte the difference from a specified time period's mean of each 
+       element in a list .
+    
+    Parameters:
+    b  -- building record
+    p  -- Period (in hours) in which to divide the time series.
+    
+    Returns:
+    List of len(t) in which each difference from mean is represented.
+    """
+    is_midnight     = (lambda x: x.hour == 0)
+    pers, new_times = get_periods(b, 24, is_midnight, "kwhs")
+    pers = np.ma.filled(pers)
+    avg_pers        = np.average(pers, axis = 1)
+    for avg, p in zip(avg_pers, pers):
+            p -= avg
+    return pers.reshape(-1)
+
 
 if __name__ == "__main__":
+    #Example for get_stats
     data, desc = qload("agentis_b_records_2011_updated_small.pkl")
     #data, desc = qload("agentis_b_records_2011_updated.pkl")
     sys.stdout.flush()
@@ -138,3 +159,8 @@ if __name__ == "__main__":
     for ind, d in enumerate(data):
         print get_stats(d)
         sys.stdout.flush()
+        
+    #Example for diff_mean
+    b = {"kwhs":[np.array(range(1,49)),data["kwhs"][1][-48:]], "temps":[np.array(range(1,49)),
+"nada"], "times": data["times"][-48:]}
+    diff_mean(b,24)
