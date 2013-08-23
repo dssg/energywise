4545d0adb490e2d579b444692ec8dec363f464f1
Salfeld <salfeld@cs.wisc.edu>
2013-08-23 17:48:16 -0500

    Removed show versions

--- Code/utils.py
+++ Code/utils.py
@@ -184,19 +184,3 @@ def fill_in(ts, all_times):
                 ori_flag.append(False)
     return toR, ori_flag
 
-def print_versions():
-    import types
-    unversioned = []
-    print "Modules with __version__:"
-    for val in globals().values():
-        if isinstance(val, types.ModuleType):
-            try:
-                ver = val.__version__
-                print "\t%s %s" %((val.__name__ + ":").ljust(15), ver.rjust(10))
-            except:
-                unversioned.append(val.__name__)
-
-    print "\nModules with no __version__:"
-    for mod in unversioned:
-        print "\t%s" % mod
-            
